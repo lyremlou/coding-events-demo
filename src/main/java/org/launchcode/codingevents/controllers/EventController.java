60d77e9e2a12e7fc51391d41bd059699d90f64ac
marysanders <lyremary@yahoo.com>
2023-08-01 10:26:50 -0500

    Add display and process edit form controllers, add edit template

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -2,10 +2,7 @@ package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +33,16 @@ public class EventController {
     public String processCreateEventForm(@RequestParam String eventName) {
         events.add(eventName);
         return "redirect:";
+    }
+
+    @GetMapping("edit/{eventId}")
+    public String displayEditForm(Model model, @PathVariable int eventId) {
+
+    }
+
+    @PostMapping("edit")
+    public String processEditForm(int eventId, String name, String description) {
+
     }
 
 }
